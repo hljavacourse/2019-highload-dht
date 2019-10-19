8ddeb779dd840f89617649fa9e1725111aa19dc3
hljavacourse <56443685+hljavacourse@users.noreply.github.com>
2019-10-19 22:32:52 +0300

    Update RockDBNewExceptionLite.java

--- src/main/java/ru/mail/polis/dao/impl/RockDBNewExceptionLite.java
+++ src/main/java/ru/mail/polis/dao/impl/RockDBNewExceptionLite.java
@@ -3,7 +3,7 @@ package ru.mail.polis.dao.impl;
 import java.io.IOException;
 
 public class RockDBNewExceptionLite extends IOException {
-    public RockDBNewExceptionLite (final String message, final Throwable cause) {
+    public RockDBNewExceptionLite(final String message, final Throwable cause) {
         super(message, cause);
     }
 }
