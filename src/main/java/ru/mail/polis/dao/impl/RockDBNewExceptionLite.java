f1249f52295ca78a17639c6cfccd53be3f2068f1
hljavacourse <hljavacourse@protonmail.com>
2019-10-26 23:18:03 +0300

    1comm 3 and nxt ..

--- src/main/java/ru/mail/polis/dao/impl/RockDBNewExceptionLite.java
+++
@@ -1,9 +0,0 @@
-package ru.mail.polis.dao.impl;
-
-import java.io.IOException;
-
-public class RockDBNewExceptionLite extends IOException {
-    public RockDBNewExceptionLite(final String message, final Throwable cause) {
-        super(message, cause);
-    }
-}
