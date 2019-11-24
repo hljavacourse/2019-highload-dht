f893d8974392414368e193c37a333e22feb01e3e
hljavacourse <hljavacourse@github.com>
2019-11-23 19:03:59 -0800

    add report for stage-7 vers1.0

--- src/main/java/ru/mail/polis/service/hljavacourse/AmmoGenerator.java
+++ src/main/java/ru/mail/polis/service/hljavacourse/AmmoGenerator.java
@@ -2,20 +2,24 @@ package ru.mail.polis.service.hljavacourse;
 
 import org.jetbrains.annotations.NotNull;
 
-import java.io.*;
-import java.nio.charset.StandardCharsets;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.util.concurrent.ThreadLocalRandom;
 
+import static java.nio.charset.StandardCharsets.US_ASCII;
+
 public class AmmoGenerator {
-    private static final int VALUE_LENGTH = 512;
+    public static final int VALUE_LENGTH = 256;
 
     @NotNull
-    private static String randomKey(){
+    private static String randomKey() {
         return Long.toHexString(ThreadLocalRandom.current().nextLong());
     }
 
     @NotNull
-    private static byte[] randomValue(){
+    private static byte[] randomValue() {
         final byte[] result = new byte[VALUE_LENGTH];
         ThreadLocalRandom.current().nextBytes(result);
         return result;
@@ -25,33 +29,33 @@ public class AmmoGenerator {
         final String key = randomKey();
         final byte[] value = randomValue();
         final ByteArrayOutputStream request = new ByteArrayOutputStream();
-        try (Writer writer = new OutputStreamWriter(request, StandardCharsets.US_ASCII)) {
+        try (Writer writer = new OutputStreamWriter(request, US_ASCII)) {
             writer.write("PUT /v0/entity?id=" + key + " HTTP/1.1\r\n");
             writer.write("Content-Length: " + value.length + "\r\n");
             writer.write("\r\n");
         }
         request.write(value);
-        System.out.write(Integer.toString(request.size()).getBytes(StandardCharsets.US_ASCII));
-        System.out.write(" put\n".getBytes(StandardCharsets.US_ASCII));
+        System.out.write(Integer.toString(request.size()).getBytes(US_ASCII));
+        System.out.write(" put\n".getBytes(US_ASCII));
         request.writeTo(System.out);
-        System.out.write("\r\n".getBytes(StandardCharsets.US_ASCII));
+        System.out.write("\r\n".getBytes(US_ASCII));
     }
 
-    public static void get() throws IOException {
+    private static void get() throws IOException {
         final String key = randomKey();
         final ByteArrayOutputStream request = new ByteArrayOutputStream();
-        try (Writer writer = new OutputStreamWriter(request, StandardCharsets.US_ASCII)) {
+        try (Writer writer = new OutputStreamWriter(request, US_ASCII)) {
             writer.write("GET /v0/entity?id=" + key + " HTTP/1.1\r\n");
             writer.write("\r\n");
         }
-        System.out.write(Integer.toString(request.size()).getBytes(StandardCharsets.US_ASCII));
-        System.out.write(" get\n".getBytes(StandardCharsets.US_ASCII));
+        System.out.write(Integer.toString(request.size()).getBytes(US_ASCII));
+        System.out.write(" get\n".getBytes(US_ASCII));
         request.writeTo(System.out);
-        System.out.write("\r\n".getBytes(StandardCharsets.US_ASCII));
+        System.out.write("\r\n".getBytes(US_ASCII));
     }
 
     public static void main(String[] args) throws IOException {
-        if(args.length != 2) {
+        if (args.length != 2) {
             System.err.println("Usage:\n\tjava -cp build/classes/java/main ru.mail.polis.service.<login>.AmmoGenerator <put|get> <requests>");
             System.exit(-1);
         }
@@ -59,14 +63,14 @@ public class AmmoGenerator {
         final String mode = args[0];
         final int requests = Integer.parseInt(args[1]);
 
-        switch (mode){
+        switch (mode) {
             case "put":
-                for(int i = 0; i < requests; i++) {
+                for (int i = 0; i < requests; i++) {
                     put();
                 }
                 break;
             case "get":
-                for(int i = 0; i < requests; i++) {
+                for (int i = 0; i < requests; i++) {
                     get();
                 }
                 break;
